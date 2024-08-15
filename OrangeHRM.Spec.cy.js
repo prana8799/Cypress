875edc4e2a06b9119e7ec9436eaf43782aa5c599
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-08-15 13:13:17 +0530

    Create OrangeHRM.Spec.cy.js

---
+++ OrangeHRM.Spec.cy.js
@@ -0,0 +1,22 @@
+describe('test', () => {
+    it('login', () => {
+      cy.visit('/', { failOnStatusCode: false });
+      cy.wait(2000);
+      cy.get("#username").type('Admin');
+      cy.wait(2000);
+      cy.get('#password').type('admin123');
+      cy.wait(2000);
+      cy.get('.login-button').click();
+      cy.wait(2000);
+  
+      cy.get('.oxd-main-menu-item.active').should('contain', 'Time').click();
+  
+      cy.get("[placeholder='Type for hints...']").type('akhil');
+  
+      cy.get('.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space').click();
+  
+      cy.get('.oxd-table-cell.oxta-center').should('contain', 'akhil');
+  
+      console.log("Able to view the entered employee name");
+    });
+  });
